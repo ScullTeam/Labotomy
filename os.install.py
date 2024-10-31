3f2e711ba8d792dbe54f6b502763d25d7347e3a1
ScullTeam_Official <121667818+ScullTeam@users.noreply.github.com>
2024-11-01 00:51:25 +0100

    Add files via upload

---
+++ os.install.py
@@ -0,0 +1,9 @@
+import os
+#Установка модулей
+os.system("pip install requests")
+os.system("pip install pystyle")
+os.system("pip install colored")
+os.system("pip install fake-useragent")
+os.system("pip install pyfiglet")
+os.system("pip install termcolor")
+os.system("pip install pyTelegramBotAPI")
