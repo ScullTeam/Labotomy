56178d359c5b2e685a6c11bf3a48d46f31903342
ScullTeam_Official <121667818+ScullTeam@users.noreply.github.com>
2024-11-10 09:52:19 +0100

    Delete os.install.py

--- os.install.py
+++
@@ -1,9 +0,0 @@
-import os
-#Установка модулей
-os.system("pip install requests")
-os.system("pip install pystyle")
-os.system("pip install colored")
-os.system("pip install fake-useragent")
-os.system("pip install pyfiglet")
-os.system("pip install termcolor")
-os.system("pip install pyTelegramBotAPI")
